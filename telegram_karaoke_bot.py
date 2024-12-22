f9d28f882572d257f71b618d0e35fd249c99a18d
jh <>
2024-12-21 21:48:36 -0500

    rename telegram bot

--- telegram_karaoke_bot.py
+++ telegram_karaoke_bot.py
@@ -1,29 +1,25 @@
 #!/usr/bin/env python
 # pylint: disable=unused-argument
-# This program is dedicated to the public domain under the CC0 license.
+
 
 """
-Simple Bot to reply to Telegram messages.
-
-First, a few handler functions are defined. Then, those functions are passed to
-the Application and registered at their respective places.
-Then, the bot is started and runs until we press Ctrl-C on the command line.
-
-Usage:
-Basic Echobot example, repeats messages.
-Press Ctrl-C on the command line or send a signal to the process to stop the
-bot.
+Telegram Karaoke Bot to score performances and reward with nfts.
 """
 
 import io
 import logging
 import os
+import re
 
 from dotenv import load_dotenv
 import whisper
 
 from telegram import ForceReply, Update
 from telegram.ext import Application, CallbackContext, CommandHandler, ContextTypes, MessageHandler, filters
+from telegram.constants import ParseMode
+
+from contract_interaction import call_contract_mint
+from generate_nft import create_upload_nft
 
 load_dotenv()
 
@@ -39,6 +35,9 @@ logging.getLogger("httpx").setLevel(logging.WARNING)
 logger = logging.getLogger(__name__)
 
 transcriber = whisper.load_model('medium.en')
+
+# Maps user id to etherium wallet address
+nft_wallets = {}
 
 
 # Define a few command handlers. These usually take the two arguments update and
@@ -56,6 +55,26 @@ async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> No
     """Send a message when the command /help is issued."""
     await update.message.reply_text("Help!")
 
+async def register_wallet_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Send a message when the command /register is issued."""
+    cmd = update.message.text.strip()
+    addr = cmd.removeprefix('/register').strip()
+    logging.info(f"received address: '{addr}'")
+
+    if re.match(r"^0x[a-fA-F0-9]{40}$", addr):
+        nft_wallets[update.effective_user.id] = addr
+        await update.message.reply_text(f"Wallet address registered successfully: {addr}")
+    else:
+        await update.message.reply_text("Invalid wallet address format. Please use the format 0x...")
+
+async def get_wallet_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Send a message when the command /register is issued."""
+    addr = nft_wallets.get(update.effective_user.id)
+    if addr:
+        await update.message.reply_text(f"Wallet address registered: {addr}")
+    else:
+        await update.message.reply_text("No wallets registered yet. Please use command: /register 0x...")
+
 
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Echo the user message."""
@@ -63,6 +82,12 @@ async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
 
 async def get_voice(update: Update, context: CallbackContext) -> None:
+    addr = nft_wallets.get(update.effective_user.id)
+    if not addr:
+        await update.message.reply_text("No wallets registered yet")
+        return
+
+
     logging.info(f"Received voice message: {update.message.voice.file_id}")
     voice = update.message.voice
     voice_file = await context.bot.get_file(voice.file_id)
@@ -72,7 +97,22 @@ async def get_voice(update: Update, context: CallbackContext) -> None:
 
     results = transcriber.transcribe('voice.ogg')
     logging.info(f"transcription results: {results}")
+
     await update.message.reply_text(f"nice voice! I think you said: {results['text']}")
+
+    logging.info(f"generating nft image and metadata")
+    json_cid = create_upload_nft(results['text'], 'test song')
+
+    logging.info(f"minting nft for {json_cid}")
+    receipt = call_contract_mint(addr, f"ipfs://{json_cid}")
+
+    if receipt:
+        txn_hash = receipt['transactionHash'].to_0x_hex()
+        logging.info(f"minting nft receipt tx: {txn_hash}")
+        await update.message.reply_text(
+            f"minted nft for you: [{txn_hash}](https://sepolia.etherscan.io/tx/{txn_hash})",
+            parse_mode=ParseMode.MARKDOWN_V2,
+        )
 
 
 def main() -> None:
@@ -83,6 +123,8 @@ def main() -> None:
     # on different commands - answer in Telegram
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("help", help_command))
+    application.add_handler(CommandHandler("register", register_wallet_command))
+    application.add_handler(CommandHandler("current_wallet", get_wallet_command))
 
     # on non command i.e message - echo the message on Telegram
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
